f78229907661f81d30a710304275deb690167cb1
letyresina <leticiaresina@gmail.com>
2023-05-12 10:28:00 -0300

    Adicionado animação de escrita no scroll de músicas (decidir se vai ficar aqui ou no titulo daisy jones

--- script/script.js
+++ script/script.js
@@ -4,3 +4,17 @@ const secaoAlvo = document.getElementById('conteudo-home');
 paraScrollar.addEventListener('click', function() {
     secaoAlvo.scrollIntoView({ behavior: 'smooth' });
 });
+
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+  });
+}
+
+const musicas = document.querySelector('.musicas-scroll');
+typeWrite(musicas, "<br>");
