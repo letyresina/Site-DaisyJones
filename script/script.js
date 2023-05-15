72e97646866b2bbf5b00ea0e9bad5242dcbbb104
letyresina <leticiaresina@gmail.com>
2023-05-15 19:50:32 -0300

    Feito a página home!

--- script/script.js
+++ script/script.js
@@ -26,3 +26,45 @@ typeWrite(titulo);
 function abrirIntegrantes(){
     window.open("integrantes.html")
 }
+
+// Carrossel de resenha
+document.addEventListener('DOMContentLoaded', function() {
+    var slides = document.querySelectorAll('.carousel-slide');
+    var currentSlide = 0;
+    var slideInterval = setInterval(nextSlide, 3000);
+    var next = document.querySelector('.next');
+    var prev = document.querySelector('.prev');
+    var playing = true;
+    var pauseButton = document.querySelector('#pause');
+    var playButton = document.querySelector('#play');
+  
+    function nextSlide() {
+      slides[currentSlide].classList.remove('active');
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].classList.add('active');
+    }
+  
+    function prevSlide() {
+      slides[currentSlide].classList.remove('active');
+      currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+      slides[currentSlide].classList.add('active');
+    }
+  
+    next.addEventListener('click', function() {
+      nextSlide();
+      if (playing) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, 3000);
+      }
+    });
+  
+    prev.addEventListener('click', function() {
+      prevSlide();
+      if (playing) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, 3000);
+      }
+    });
+    
+  });
+  
